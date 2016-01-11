8375cf6a3606e2ac2edbb78398c9e2b80e6772b1
Alex Ling <hkalexling@gmail.com>
2016-01-12 04:58:31 +0800

    Remove unnecessary imports

--- KonaBot/KonaBot-Bridging-Header.h
+++ KonaBot/KonaBot-Bridging-Header.h
@@ -6,9 +6,6 @@
 //  Copyright © 2015 Alex Ling. All rights reserved.
 //
 
-//#import "AFNetworking.h"
-#import <libxml/HTMLtree.h>
-#import <libxml/xpathInternals.h>
 #import "JTSImageInfo.h"
 #import "JTSImageViewController.h"
 #import "RZSquaresLoading.h"
