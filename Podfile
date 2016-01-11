4c3257393e96bf3f64b78ec4cd230add324b3974
Alex Ling <hkalexling@gmail.com>
2016-01-12 02:18:38 +0800

    Switch to Cocoapod

---
+++ Podfile
@@ -0,0 +1,10 @@
+use_frameworks!
+platform :ios, '8.0'
+
+pod 'Kanna', '~> 1.0.0'
+pod 'AFNetworking', '~> 3.0'
+
+target 'KonaBot' do
+
+end
+
---
+++ Pods/Target Support Files/Pods-KonaBot/Pods-KonaBot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KonaBot : NSObject
+@end
+@implementation PodsDummy_Pods_KonaBot
+@end
