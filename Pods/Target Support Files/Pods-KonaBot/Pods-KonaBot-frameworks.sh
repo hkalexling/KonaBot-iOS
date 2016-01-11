09c6cd83ec9bdc39b5b53f6cfbea124d4c7cba14
Alex Ling <hkalexling@gmail.com>
2016-01-12 04:51:01 +0800

    wrong

--- Pods/Target Support Files/Pods-KonaBot/Pods-KonaBot-frameworks.sh
+++ Pods/Target Support Files/Pods-KonaBot/Pods-KonaBot-frameworks.sh
@@ -53,9 +53,11 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'Pods-KonaBot/AFNetworking.framework'
+  install_framework 'Pods-KonaBot/JTSImageViewController.framework'
   install_framework 'Pods-KonaBot/Kanna.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'Pods-KonaBot/AFNetworking.framework'
+  install_framework 'Pods-KonaBot/JTSImageViewController.framework'
   install_framework 'Pods-KonaBot/Kanna.framework'
 fi
