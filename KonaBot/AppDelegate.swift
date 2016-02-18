825d17554024a5530eecab28fac9cc3194876c1f
Alex Ling <hkalexling@gmail.com>
2016-02-18 16:31:27 +0800

    Move the cache deletion task from applicationWillTerminate: to applicationDidEnterBackground:

--- KonaBot/AppDelegate.swift
+++ KonaBot/AppDelegate.swift
@@ -70,8 +70,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	}
 
 	func applicationDidEnterBackground(application: UIApplication) {
-		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+		self.yuno.deleteEntity("Preview")
+		if NSUserDefaults.standardUserDefaults().boolForKey("optimize"){
+			self.yuno.deleteEntity("Cache")
+		}
 	}
 
 	func applicationWillEnterForeground(application: UIApplication) {
@@ -80,13 +82,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func applicationDidBecomeActive(application: UIApplication) {
 		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-	}
-
-	func applicationWillTerminate(application: UIApplication) {
-		self.yuno.deleteEntity("Preview")
-		if NSUserDefaults.standardUserDefaults().boolForKey("optimize"){
-			self.yuno.deleteEntity("Cache")
-		}
 	}
 	
 	// MARK: - Core Data stack
